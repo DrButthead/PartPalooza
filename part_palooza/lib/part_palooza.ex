7f297020a47efa37266cbbbe237112fc21a7acfc
Oliver <oliver.olding@hotmail.com>
2024-03-19 22:06:13 +0000

    Setting up the project

---
+++ part_palooza/lib/part_palooza.ex
@@ -0,0 +1,18 @@
+defmodule PartPalooza do
+  @moduledoc """
+  Documentation for `PartPalooza`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PartPalooza.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ part_palooza/test/part_palooza_test.exs
@@ -0,0 +1,8 @@
+defmodule PartPaloozaTest do
+  use ExUnit.Case
+  doctest PartPalooza
+
+  test "greets the world" do
+    assert PartPalooza.hello() == :world
+  end
+end
